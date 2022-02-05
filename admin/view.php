da0ad064b6b121e52f2fe3639e562a36d52178ea
icarta-l <cartaidan@gmail.com>
2022-02-05 13:23:05 +0100

    Refactorise code

--- admin/view.php
+++ admin/view.php
@@ -1,11 +1,10 @@
 <?php 
 require_once str_replace("admin", "functions.php", __DIR__);
 
-global $wpdb;
 ?>
 <div class="wrap">
 	<h1><?= __('Stop Confusion', 'stop_confusion') ?></h1>
 	<?php
-	check_all_themes();
+	handle_themes();
 	?>
 </div>
